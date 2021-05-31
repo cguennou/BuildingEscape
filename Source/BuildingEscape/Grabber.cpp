c7c04fef446ae31c7e131f2c0a8d54a11324bc75
Chloé <c.guennou@gmail.com>
2021-05-31 02:00:16 +0200

    Default pawn

---
+++ Source/BuildingEscape/Grabber.cpp
@@ -0,0 +1,45 @@
+// @CGlearning
+
+
+#include "Grabber.h"
+
+// Sets default values for this component's properties
+UGrabber::UGrabber()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UGrabber::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UE_LOG(LogTemp, Warning, TEXT("Grabber reporting for duty"));
+	
+}
+
+
+// Called every frame
+void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	//Get Player viewpoint
+	FVector PlayerViewPointLocation;
+	FRotator PlayerViewPointRotation;
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+		OUT PlayerViewPointLocation,
+		OUT PlayerViewPointRotation);
+
+	UE_LOG(LogTemp, Warning, TEXT("The player viewpoint location is : %s"), *PlayerViewPointLocation.ToString());
+	UE_LOG(LogTemp, Warning, TEXT("The player viewpoint rotation is : %s"), *PlayerViewPointRotation.ToString());
+	//Ray-Cast out to a certain distance (Reach)
+
+	//See what it hits
+}
+
---
+++ Source/BuildingEscape/Grabber.h
@@ -0,0 +1,31 @@
+// @CGlearning
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "GameFramework/PlayerController.h"
+#include "Engine/World.h"
+#include "Grabber.generated.h"
+
+#define OUT
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BUILDINGESCAPE_API UGrabber : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UGrabber();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+		
+};
